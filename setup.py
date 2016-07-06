08b911c9ccd5f5684041475693852b8d7d0cff84
Zach Panter <zachpanter@gmail.com>
2016-07-06 11:02:13 -0600

    Added setup.py to root folder

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setuptools
+
+setup(
+    name='taskengine',
+    version='0.1',
+    description='A microservices-architecture productivity and agile project-management app.'
+)
+
+__author__ = 'Zach Panter'
