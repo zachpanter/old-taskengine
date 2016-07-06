3b00564c4e0a431260420df32243747b74e1db02
Zach Panter <zachpanter@gmail.com>
2016-07-06 11:04:28 -0600

    Put create note functionality into a function

--- starterScript.py
+++ starterScript.py
@@ -64,51 +64,57 @@ while True:
 
 
 
+def create_note()
+	"""
+	PURPOSE:
+	PARAMETERS:
+	RETURNS:
+	"""
 
-# To create a new note, simply create a new Note object and fill in
-# attributes such as the note's title.
-note = Types.Note()
-note.title = "Test note 2 from EDAMTest.py"
+	# To create a new note, simply create a new Note object and fill in
+	# attributes such as the note's title.
+	note = Types.Note()
+	note.title = "Test note 2 from EDAMTest.py"
 
-# To include an attachment such as an image in a note, first create a Resource
-# for the attachment. At a minimum, the Resource contains the binary attachment
-# data, an MD5 hash of the binary data, and the attachment MIME type.
-# It can also include attributes such as filename and location.
-image = open('enlogo.png', 'rb').read()
-md5 = hashlib.md5()
-md5.update(image)
-hash = md5.digest()
+	# To include an attachment such as an image in a note, first create a Resource
+	# for the attachment. At a minimum, the Resource contains the binary attachment
+	# data, an MD5 hash of the binary data, and the attachment MIME type.
+	# It can also include attributes such as filename and location.
+	image = open('enlogo.png', 'rb').read()
+	md5 = hashlib.md5()
+	md5.update(image)
+	hash = md5.digest()
 
-data = Types.Data()
-data.size = len(image)
-data.bodyHash = hash
-data.body = image
+	data = Types.Data()
+	data.size = len(image)
+	data.bodyHash = hash
+	data.body = image
 
-resource = Types.Resource()
-resource.mime = 'image/png'
-resource.data = data
+	resource = Types.Resource()
+	resource.mime = 'image/png'
+	resource.data = data
 
-# Now, add the new Resource to the note's list of resources
-note.resources = [resource]
+	# Now, add the new Resource to the note's list of resources
+	note.resources = [resource]
 
-# To display the Resource as part of the note's content, include an <en-media>
-# tag in the note's ENML content. The en-media tag identifies the corresponding
-# Resource using the MD5 hash.
-hash_hex = binascii.hexlify(hash)
+	# To display the Resource as part of the note's content, include an <en-media>
+	# tag in the note's ENML content. The en-media tag identifies the corresponding
+	# Resource using the MD5 hash.
+	hash_hex = binascii.hexlify(hash)
 
-# The content of an Evernote note is represented using Evernote Markup Language
-# (ENML). The full ENML specification can be found in the Evernote API Overview
-# at http://dev.evernote.com/documentation/cloud/chapters/ENML.php
-note.content = '<?xml version="1.0" encoding="UTF-8"?>'
-note.content += '<!DOCTYPE en-note SYSTEM ' \
-    '"http://xml.evernote.com/pub/enml2.dtd">'
-note.content += '<en-note>Here is the Evernote logo:<br/>'
-note.content += '<en-media type="image/png" hash="' + hash_hex + '"/>'
-note.content += '</en-note>'
+	# The content of an Evernote note is represented using Evernote Markup Language
+	# (ENML). The full ENML specification can be found in the Evernote API Overview
+	# at http://dev.evernote.com/documentation/cloud/chapters/ENML.php
+	note.content = '<?xml version="1.0" encoding="UTF-8"?>'
+	note.content += '<!DOCTYPE en-note SYSTEM ' \
+		'"http://xml.evernote.com/pub/enml2.dtd">'
+	note.content += '<en-note>Here is the Evernote logo:<br/>'
+	note.content += '<en-media type="image/png" hash="' + hash_hex + '"/>'
+	note.content += '</en-note>'
 
-# Finally, send the new note to Evernote using the createNote method
-# The new Note object that is returned will contain server-generated
-# attributes such as the new note's unique GUID.
-created_note = note_store.createNote(note)
+	# Finally, send the new note to Evernote using the createNote method
+	# The new Note object that is returned will contain server-generated
+	# attributes such as the new note's unique GUID.
+	created_note = note_store.createNote(note)
 
-print "Successfully created a new note with GUID: ", created_note.guid
+	print "Successfully created a new note with GUID: ", created_note.guid
